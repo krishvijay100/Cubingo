354400103457b53ffee939a808eea9625a62c5ce
k4vijay <k4vijay@uwaterloo.ca>
2025-12-20 22:54:47 -0500

    switched oll and pll pages from displaying statically through html files to dynamically through js

---
+++ algorithms.js
@@ -0,0 +1,629 @@
+//PLL's
+
+const ubperm = {
+    id: 'ub-perm',
+    name: 'Ub Perm',
+    notation: "R' U R' U' R3 U' R' U R U R2",
+    category: 'PLL',
+    youtubeId: 'mssHbM4JYOc'
+}
+
+const uaperm = {
+    id: 'ua-perm',
+    name: 'Ua Perm',
+    notation: "R U R' U R' U' R2 U' R' U R' U R",
+    category: 'PLL',
+    youtubeId: 'WNkmbgnvQPU'
+}
+
+const zperm = {
+    id: 'z-perm',
+    name: 'Z Perm',
+    notation: "(M2 U') (M2 U') M' U2 M2 U2 M'",
+    category: 'PLL',
+    youtubeId: 'I0haU9Z2MeA'
+}
+
+const hperm = {
+  id: 'h-perm',
+  name: 'H Perm',
+  notation: "(M2 U' M2) U2 (M2 U' M2)",
+  category: 'PLL',
+  youtubeId: 'pmA8ToRfOXQ'
+}
+
+const aaperm = {
+  id: 'aa-perm',
+  name: 'Aa Perm',
+  notation: "x R' U R' D2 R U' R' D2 R2 x'",
+  category: 'PLL',
+  youtubeId: 'ibDaGbBp1wY'
+}
+
+const abperm = {
+  id: 'ab-perm',
+  name: 'Ab Perm',
+  notation: "x R2 D2 R U R' D2 R U' R x'",
+  category: 'PLL',
+  youtubeId: 'Nd0NvF4PF-g'
+}
+
+const eperm = {
+  id: 'e-perm',
+  name: 'E Perm',
+  notation: "x' (R U' R' D) (R U R' D') (R U R' D) (R U' R' D') x",
+  category: 'PLL',
+  youtubeId: 'ifo0uz8s4cI'
+}
+
+const gaperm = {
+  id: 'ga-perm',
+  name: 'Ga Perm',
+  notation: "R2 U R' U R' U' R U' R2 (D U') R' U R D'",
+  category: 'PLL',
+  youtubeId: 'wd4gd8B7QSA'
+}
+
+const gbperm = {
+  id: 'gb-perm',
+  name: 'Gb Perm',
+  notation: "D R' U' R (U D') R2 U R' U R U' R U' R2",
+  category: 'PLL',
+  youtubeId: '8fJRWcKSMUk'
+}
+
+const gcperm = {
+  id: 'gc-perm',
+  name: 'Gc Perm',
+  notation: "R2 U' R U' R U R' U R2 (D' U) R U' R' D",
+  category: 'PLL',
+  youtubeId: 'L8C6nY-Qy3E'
+}
+
+const gdperm = {
+  id: 'gd-perm',
+  name: 'Gd Perm',
+  notation: "D' R U R' (U' D) R2 U' R U' R' U R' U R2",
+  category: 'PLL',
+  youtubeId: 'ESv6a7Cid6c'
+}
+
+const raperm = {
+  id: 'ra-perm',
+  name: 'Ra Perm',
+  notation: "(R U' R' U') (R U R D) (R' U' R D') R' U2 R'",
+  category: 'PLL',
+  youtubeId: 'IGHfp6CgfaA'
+}
+
+const rbperm = {
+  id: 'rb-perm',
+  name: 'Rb Perm',
+  notation: "(R' U2) (R U2') R' F (R U R' U') R' F' R2",
+  category: 'PLL',
+  youtubeId: 'FwXr8Cawxfg'
+}
+
+const japerm = {
+  id: 'ja-perm',
+  name: 'Ja Perm',
+  notation: "(R' U L') U2 (R U' R') U2 R L",
+  category: 'PLL',
+  youtubeId: 'nh9th-AYF_8'
+}
+
+const jbperm = {
+  id: 'jb-perm',
+  name: 'Jb Perm',
+  notation: "(R U R' F') (R U R' U') R' F R2 U' R'",
+  category: 'PLL',
+  youtubeId: 'QXIIxGzMftw'
+}
+
+const tperm = {
+  id: 't-perm',
+  name: 'T Perm',
+  notation: "(R U R' U') R' F R2 U' R' U' (R U R' F')",
+  category: 'PLL',
+  youtubeId: 'PX1R7VW9YTs'
+}
+
+const fperm = {
+  id: 'f-perm',
+  name: 'F Perm',
+  notation: "R' U' F' (R U R' U') R' F (R2 U' R' U') (R U R') U R",
+  category: 'PLL',
+  youtubeId: 'KLL-Qt9Lm90'
+}
+
+const vperm = {
+  id: 'v-perm',
+  name: 'V Perm',
+  notation: "(R' U R' U') y R' F' R2 U' R' U R' F R F",
+  category: 'PLL',
+  youtubeId: 'Ud9ZDcS2yRk'
+}
+
+const yperm = {
+  id: 'y-perm',
+  name: 'Y Perm',
+  notation: "F R U' R' U' R U R' F' (R U R' U') (R' F R F')",
+  category: 'PLL',
+  youtubeId: 'gC3LvhSAXSA'
+}
+
+const naperm = {
+  id: 'na-perm',
+  name: 'Na Perm',
+  notation: "F' (R U R' U') R' F R2 F (U' R' U') R U F' R'",
+  category: 'PLL',
+  youtubeId: 'iNfx--vOxd0'
+}
+
+const nbperm = {
+  id: 'nb-perm',
+  name: 'Nb Perm',
+  notation: "r' D' F (r U' r)' F' D r2 U r' U' (r' F r F')",
+  category: 'PLL',
+  youtubeId: 'P5yMlQQ3nsM'
+}
+
+//OLL's
+const oll1 = {
+  id: 'oll-1',
+  name: 'OLL1',
+  notation: "R U2' R2 F R F' U2' (R' F R F')",
+  category: 'OLL',
+  youtubeId: 'Krt5t5X9gTo'
+}
+
+const oll2 = {
+  id: 'oll-2',
+  name: 'OLL2',
+  notation: "F (R U R' U') F' f (R U R' U') f'",
+  category: 'OLL',
+  youtubeId: 'EyCwdkn03sM'
+}
+
+const oll3 = {
+  id: 'oll-3',
+  name: 'OLL3',
+  notation: "f (R U R' U') f' U' F (R U R' U') F'",
+  category: 'OLL',
+  youtubeId: 'KdgjbaG0YmI'
+}
+
+const oll4 = {
+  id: 'oll-4',
+  name: 'OLL4',
+  notation: "f (R U R' U' f') U F (R U R' U') F'",
+  category: 'OLL',
+  youtubeId: 'JlGs-0-RxJk'
+}
+
+const oll5 = {
+  id: 'oll-5',
+  name: 'OLL5',
+  notation: "r' U2 R U R' U r",
+  category: 'OLL',
+  youtubeId: 'zDAhS61IFlw'
+}
+
+const oll6 = {
+  id: 'oll-6',
+  name: 'OLL6',
+  notation: "r U2 R' U' R U' r'",
+  category: 'OLL',
+  youtubeId: 'xtuAlp8AAj8'
+}
+
+const oll7 = {
+  id: 'oll-7',
+  name: 'OLL7',
+  notation: "r U R' U R U2 r'",
+  category: 'OLL',
+  youtubeId: 'Ls-uPCQbjqg'
+}
+
+const oll8 = {
+  id: 'oll-8',
+  name: 'OLL8',
+  notation: "r' U' R U' R' U2 r",
+  category: 'OLL',
+  youtubeId: 'Vptj1rtxcxA'
+}
+const oll9 = {
+  id: 'oll-9',
+  name: 'OLL9',
+  notation: "(R U R' U') R' F R2 U R' U' F'",
+  category: 'OLL',
+  youtubeId: '1tkJ78gxtfU'
+}
+
+const oll10 = {
+  id: 'oll-10',
+  name: 'OLL10',
+  notation: "(R U R' U) (R' F R F') R U2 R'",
+  category: 'OLL',
+  youtubeId: 'RPjJagkkvwM'
+}
+
+const oll11 = {
+  id: 'oll-11',
+  name: 'OLL11',
+  notation: "r' R2 U R' U R U2 R' U M'",
+  category: 'OLL',
+  youtubeId: 'YsNvf0oUw9U'
+}
+
+const oll12 = {
+  id: 'oll-12',
+  name: 'OLL12',
+  notation: "r R2' U' R U' R' U2 R U' M",
+  category: 'OLL',
+  youtubeId: 'E5WsvlCzOrM'
+}
+
+const oll13 = {
+  id: 'oll-13',
+  name: 'OLL13',
+  notation: "F U (R U2 R') (U' R U R') F'",
+  category: 'OLL',
+  youtubeId: 'Xz96dRD2DaI'
+}
+
+const oll14 = {
+  id: 'oll-14',
+  name: 'OLL14',
+  notation: "R' F R U R' F' R F U' F'",
+  category: 'OLL',
+  youtubeId: 'xEoC4d91yAc'
+}
+
+const oll15 = {
+  id: 'oll-15',
+  name: 'OLL15',
+  notation: "(r' U' r) (R' U' R U) (r' U r)",
+  category: 'OLL',
+  youtubeId: 'VeG5V1a6pHA'
+}
+
+const oll16 = {
+  id: 'oll-16',
+  name: 'OLL16',
+  notation: "(r U r') (R U R' U') (r U' r')",
+  category: 'OLL',
+  youtubeId: 'Gar0BFyHWXs'
+}
+
+const oll17 = {
+  id: 'oll-17',
+  name: 'OLL17',
+  notation: "(R U R' U) (R' F R F') U2 (R' F R F')",
+  category: 'OLL',
+  youtubeId: '4QtUb6tleZE'
+}
+
+const oll18 = {
+  id: 'oll-18',
+  name: 'OLL18',
+  notation: "R U2 R2 F R F' U2 M' U R U' r'",
+  category: 'OLL',
+  youtubeId: '2jrDoX0AQCc'
+}
+
+const oll19 = {
+  id: 'oll-19',
+  name: 'OLL19',
+  notation: "r' R U R U R' U' r R2 F R F'",
+  category: 'OLL',
+  youtubeId: 'OUTQriBA888'
+}
+
+const oll20 = {
+  id: 'oll-20',
+  name: 'OLL20',
+  notation: "r' R U R U R' U' M2 U R U' r'",
+  category: 'OLL',
+  youtubeId: 'Mlfmfv8KFOU'
+}
+
+const oll21 = {
+  id: 'oll-21',
+  name: 'OLL21',
+  notation: "(R U R' U) (R U' R' U) R U2 R'",
+  category: 'OLL',
+  youtubeId: 'LkLCxmV63Sc'
+}
+
+const oll22 = {
+  id: 'oll-22',
+  name: 'OLL22',
+  notation: "R U2 (R2 U') (R2 U') R2 U2 R",
+  category: 'OLL',
+  youtubeId: 'Z6SYc0MyBnk'
+}
+
+const oll23 = {
+  id: 'oll-23',
+  name: 'OLL23',
+  notation: "R2 D R' U2 R D' R' U2 R'",
+  category: 'OLL',
+  youtubeId: 'YvgWA5n7-y0'
+}
+
+const oll24 = {
+  id: 'oll-24',
+  name: 'OLL24',
+  notation: "r U R' U' r' F R F'",
+  category: 'OLL',
+  youtubeId: '5pmKYnWCNiI'
+}
+
+const oll25 = {
+  id: 'oll-25',
+  name: 'OLL25',
+  notation: "F' r U R' U' r' F R",
+  category: 'OLL',
+  youtubeId: 'w2pqzFn0KqU'
+}
+
+const oll26 = {
+  id: 'oll-26',
+  name: 'OLL26',
+  notation: "R U2 R' U' R U' R'",
+  category: 'OLL',
+  youtubeId: 'W4E8fo6-D84'
+}
+
+const oll27 = {
+  id: 'oll-27',
+  name: 'OLL27',
+  notation: "R U R' U R U2 R'",
+  category: 'OLL',
+  youtubeId: '83GLN0XmCNA'
+}
+
+const oll28 = {
+  id: 'oll-28',
+  name: 'OLL28',
+  notation: "r U R' U' M U R U' R'",
+  category: 'OLL',
+  youtubeId: 'blOdiGsmlQQ'
+}
+
+const oll29 = {
+  id: 'oll-29',
+  name: 'OLL29',
+  notation: "(R U R' U') (R U' R') (F' U' F) (R U R')",
+  category: 'OLL',
+  youtubeId: 'xaSQb6RdIrY'
+}
+
+const oll30 = {
+  id: 'oll-30',
+  name: 'OLL30',
+  notation: "r' D' r U' r' D r2 U' r' U r U r'",
+  category: 'OLL',
+  youtubeId: 'HSDYQ3F9_GQ'
+}
+
+const oll31 = {
+  id: 'oll-31',
+  name: 'OLL31',
+  notation: "R' U' F U R U' R' F' R",
+  category: 'OLL',
+  youtubeId: 'd5krfXeAvZ8'
+}
+
+const oll32 = {
+  id: 'oll-32',
+  name: 'OLL32',
+  notation: "S R U R' U' R' F R f'",
+  category: 'OLL',
+  youtubeId: 'fK4goEB_xPg'
+}
+
+const oll33 = {
+  id: 'oll-33',
+  name: 'OLL33',
+  notation: "(R U R' U') (R' F R F')",
+  category: 'OLL',
+  youtubeId: 'yw6EhRi2Ylo'
+}
+
+const oll34 = {
+  id: 'oll-34',
+  name: 'OLL34',
+  notation: "R U R2 U' R' F R U R U' F'",
+  category: 'OLL',
+  youtubeId: 'tOB1D8nmMIs'
+}
+
+const oll35 = {
+  id: 'oll-35',
+  name: 'OLL35',
+  notation: "R U2' R2' F R F' R U2' R'",
+  category: 'OLL',
+  youtubeId: 'bG14LWhXWCk'
+}
+
+const oll36 = {
+  id: 'oll-36',
+  name: 'OLL36',
+  notation: "L' U' L U' L' U L U L F' L' F",
+  category: 'OLL',
+  youtubeId: '6E5tMt6BGog'
+}
+
+const oll37 = {
+  id: 'oll-37',
+  name: 'OLL37',
+  notation: "F R U' R' U' R U R' F'",
+  category: 'OLL',
+  youtubeId: '1uNKnyKiFOo'
+}
+
+const oll38 = {
+  id: 'oll-38',
+  name: 'OLL38',
+  notation: "R U R' U R U' R' U' (R' F R F')",
+  category: 'OLL',
+  youtubeId: '-OHDLxFILFU'
+}
+
+const oll39 = {
+  id: 'oll-39',
+  name: 'OLL39',
+  notation: "L F' L' U' L U F U' L'",
+  category: 'OLL',
+  youtubeId: 'frAvYb9HoYo'
+}
+
+const oll40 = {
+  id: 'oll-40',
+  name: 'OLL40',
+  notation: "R' F R U R' U' F' U R",
+  category: 'OLL',
+  youtubeId: 'fYZxskw2EgI'
+}
+
+const oll41 = {
+  id: 'oll-41',
+  name: 'OLL41',
+  notation: "R U R' U R U2 R' F (R U R' U') F'",
+  category: 'OLL',
+  youtubeId: 'AmxQNjhBzfk'
+}
+
+const oll42 = {
+  id: 'oll-42',
+  name: 'OLL42',
+  notation: "R' U' R U' R' U2 R F (R U R' U') F'",
+  category: 'OLL',
+  youtubeId: 'uL5R2Hx2fV8'
+}
+
+const oll43 = {
+  id: 'oll-43',
+  name: 'OLL43',
+  notation: "R' U' F' U F R",
+  category: 'OLL',
+  youtubeId: 'joYVuCv8p-E'
+}
+
+const oll44 = {
+  id: 'oll-44',
+  name: 'OLL44',
+  notation: "F U R U' R' F'",
+  category: 'OLL',
+  youtubeId: 'CeqGe0Q1wMg'
+}
+
+const oll45 = {
+  id: 'oll-45',
+  name: 'OLL45',
+  notation: "F (R U R' U') F'",
+  category: 'OLL',
+  youtubeId: '3HJQsUqEqyM'
+}
+
+const oll46 = {
+  id: 'oll-46',
+  name: 'OLL46',
+  notation: "R' U' R' F R F' U R",
+  category: 'OLL',
+  youtubeId: 'AQynWJ4dAjk'
+}
+
+const oll47 = {
+  id: 'oll-47',
+  name: 'OLL47',
+  notation: "R' U' (R' F R F') (R' F R F') U R",
+  category: 'OLL',
+  youtubeId: '9Ui1n0UjaCU'
+}
+
+const oll48 = {
+  id: 'oll-48',
+  name: 'OLL48',
+  notation: "F (R U R' U') (R U R' U') F'",
+  category: 'OLL',
+  youtubeId: '-yAt0RgMmsY'
+}
+
+const oll49 = {
+  id: 'oll-49',
+  name: 'OLL49',
+  notation: "r U' r2 U r2 U r2 U' r",
+  category: 'OLL',
+  youtubeId: 'dQrWHwyT8bo'
+}
+
+const oll50 = {
+  id: 'oll-50',
+  name: 'OLL50',
+  notation: "r' U r2 U' r2 U' r2 U r'",
+  category: 'OLL',
+  youtubeId: '0jwU3QUs93g'
+}
+
+const oll51 = {
+  id: 'oll-51',
+  name: 'OLL51',
+  notation: "F (U R U' R') (U R U' R') F'",
+  category: 'OLL',
+  youtubeId: '6j4D0FHgSOY'
+}
+
+const oll52 = {
+  id: 'oll-52',
+  name: 'OLL52',
+  notation: "R' F' U' F U' R U R' U R",
+  category: 'OLL',
+  youtubeId: 'ZuRxOkExG8o'
+}
+
+const oll53 = {
+  id: 'oll-53',
+  name: 'OLL53',
+  notation: "r' U' R U' R' U R U' R' U2 r",
+  category: 'OLL',
+  youtubeId: 'FV3aJg0kmIw'
+}
+
+const oll54 = {
+  id: 'oll-54',
+  name: 'OLL54',
+  notation: "r U R' U R U' R' U R U2 r'",
+  category: 'OLL',
+  youtubeId: 'gydKjGqInfg'
+}
+
+const oll55 = {
+  id: 'oll-55',
+  name: 'OLL55',
+  notation: "R U2 R2 U' R U' R' U2 F R F'",
+  category: 'OLL',
+  youtubeId: 'QDehoJhTOtY'
+}
+
+const oll56 = {
+  id: 'oll-56',
+  name: 'OLL56',
+  notation: "(r U r') (U R U' R') (U R U' R') (r U' r')",
+  category: 'OLL',
+  youtubeId: 'KZkE5GBzS9k'
+}
+
+const oll57 = {
+  id: 'oll-57',
+  name: 'OLL57',
+  notation: "(R U R' U') M' U R U' r'",
+  category: 'OLL',
+  youtubeId: 'qHvJd0pk41Q'
+}
+
+//arrays containing all oll & pll objects, used in the ollalgs & pllalgs html file
+const ollAlgorithms = [oll1, oll2, oll3, oll4, oll5, oll6, oll7, oll8, oll9, oll10, oll11, oll12, oll13, oll14, oll15, oll16, oll17, oll18, oll19, oll20, oll21, oll22, oll23, oll24, oll25, oll26, oll27, oll28, oll29, oll30, oll31, oll32, oll33, oll34, oll35, oll36, oll37, oll38, oll39, oll40, oll41, oll42, oll43, oll44, oll45, oll46, oll47, oll48, oll49, oll50, oll51, oll52, oll53, oll54, oll55, oll56, oll57];
+const pllAlgorithms = [ubperm, uaperm, zperm, hperm, aaperm, abperm, eperm, gaperm, gbperm, gcperm, gdperm, raperm, rbperm, japerm, jbperm, tperm, fperm, vperm, yperm, naperm, nbperm];
