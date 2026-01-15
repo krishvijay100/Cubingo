ef103ce975a3ff138d81672eac77fe60e8cc6d4b
k4vijay <k4vijay@uwaterloo.ca>
2026-01-14 21:04:53 -0500

    feat(client): add backend storage adapter

---
+++ client/src/storage/cubeStorage.js
@@ -0,0 +1,27 @@
+import { fetchMe, saveProgress } from "../api/progress.js";
+import { DEFAULT_STATE, mergeState, clone } from "./defaultState.js";
+
+async function loadState() {
+  const me = await fetchMe();
+  if (!me || !me.progress) {
+    return clone(DEFAULT_STATE);
+  }
+  return mergeState(me.progress);
+}
+
+async function saveState(state) {
+  await saveProgress(state);
+}
+
+async function resetState() {
+  await saveProgress(clone(DEFAULT_STATE));
+  return clone(DEFAULT_STATE);
+}
+
+export const CubeStorage = {
+  DEFAULT_STATE,
+  loadState,
+  saveState,
+  resetState,
+  mergeState
+};
